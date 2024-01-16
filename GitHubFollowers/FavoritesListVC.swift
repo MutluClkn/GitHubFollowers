73e7653a6b099a20061f14684222daccf19c7b3b
Mutlu Çalkan <mutlu.calkan@gmail.com>
2024-01-16 10:21:44 +0300

    Project setup has been done.

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Mutlu Çalkan on 16.01.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Mutlu Çalkan on 16.01.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
