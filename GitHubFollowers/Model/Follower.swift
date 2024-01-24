bf68ab06791ca95864cad4126dad00a2b430ef38
Mutlu Çalkan <mutlu.calkan@gmail.com>
2024-01-24 11:02:30 +0300

    Model files created for network call.

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Mutlu Çalkan on 24.01.2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Mutlu Çalkan on 24.01.2024.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
