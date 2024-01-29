8c511ea13f798238bd9c639354dde89f9d0fb82f
Mutlu Çalkan <mutlu.calkan@gmail.com>
2024-01-29 08:41:02 +0300

    Network call created to get followers.

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -15,6 +15,16 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { follower, errorMessage in
+            guard let follower = follower else {
+                self.presentGFAlertOnMainThread(title: "Error", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("Followers count = \(follower.count)")
+            print(follower)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
