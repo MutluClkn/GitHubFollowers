ee9250fba89bcda4cfab6f333f760d0ecf950be0
Mutlu Çalkan <mutlu.calkan@gmail.com>
2024-02-11 08:31:09 +0300

    UICollectionViewFlowLayout customization created.

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -32,11 +32,32 @@ class FollowerListVC: UIViewController {
     
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
+    
+    
+    ///We want to create three column flow layout.
+    ///There will be paddings on the left and right in a row.
+    ///Also, there will be item spaces between three items.
+    ///When we subtract those spaces and paddings from the view width, we will get the items.
+    ///Then we will divide them three.
+    private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
     
     private func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
