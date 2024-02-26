39407faf609786d5debc22e7cf78fb090a23a14d
Mutlu Çalkan <mutlu.calkan@gmail.com>
2024-02-26 08:03:28 +0300

    Cache used for avatar images.

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Mutlu Çalkan on 29.01.2024.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     
     private init() {}
